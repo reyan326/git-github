e6d0e73a293fcc63934dbc6e12ae14dfc44153c5
reyan326 <58131099+reyan326@users.noreply.github.com>
2021-10-08 13:19:49 +0530

    Create factor_of _number.cpp

---
+++ factor_of _number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* write program to find factor of a number */
+
+int main()
+{
+    int n, i;
+    cout<<"enter number = ",cin>>n;
+    
+    for(i=1; i<=n; i++)
+    {
+        if(n % i == 0)
+            cout<<i<<endl;
+            
+        
+        
+    }
+    
+
+    
+    
+    return 0;
+    
+}
+ 
